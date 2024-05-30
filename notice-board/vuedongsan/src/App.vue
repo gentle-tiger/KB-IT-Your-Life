a52011420f9a18ece32f560ceee0ba534988726b
Choi ho jin <jkhh9725@gmail.com>
2024-05-31 00:13:27 +0900

    notice-board

---
+++ notice-board/vuedongsan/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+const products = ref(["금천구", "중랑구", "도봉구"]);
+</script>
+
+<template>
+  <div>
+    <div>
+      <h4>{{ products[0] }} 원룬</h4>
+      <p>50만원</p>
+    </div>
+    <div>
+      <h4>{{ products[1] }} 원룬</h4>
+      <p>50만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }} 원룬</h4>
+      <p>50만원</p>
+    </div>
+  </div>
+</template>
+
+<script></script>
