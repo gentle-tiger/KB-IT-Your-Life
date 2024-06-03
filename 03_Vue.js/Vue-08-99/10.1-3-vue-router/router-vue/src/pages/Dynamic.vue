d13f1b7b3ff3faef43381e86951f4b55f8853a15
Choi ho jin <jkhh9725@gmail.com>
2024-06-03 15:24:52 +0900

    2024.06.03 Dynamic Router(useRoute, watch)

---
+++ 03_Vue.js/Vue-08-99/10.1-3-vue-router/router-vue/src/pages/Dynamic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>오늘의 기분은 {{ msg }}</h1>
+    <!-- <img :src="imgSrc" :alt="msg" /> -->
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const currentRoute = useRoute();
+// mood는 큰 객체로 사용됨
+const mood = ref(currentRoute.params.mood);
+
+// 두 개의 인자를 받음 / 자주 쓰이는 구조 /
+watch(
+  () => currentRoute.params.mood,
+  (newMood) => ((mood.value = newMood), console.log(newMood))
+);
+
+const msg = computed(() => (mood.value === 'happy' ? '행복합니다' : '신납니다.'));
+
+// const imgSrc = computed(() => (mood.value === 'happy' ? '이미지 사진1' : '이미지 사진1'));
+</script>
+
+<style scoped></style>
