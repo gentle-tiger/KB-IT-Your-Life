df3d397133b3fc1c851d7ba9c21708d37d527ca4
Choi ho jin <jkhh9725@gmail.com>
2024-06-03 15:25:53 +0900

    2024.06.03 NotFound 페이지

--- 03_Vue.js/Vue-08-99/10.1-3-vue-router/router-vue/src/App.vue
+++ 03_Vue.js/Vue-08-99/10.1-3-vue-router/router-vue/src/App.vue
@@ -6,19 +6,14 @@
       <router-link to="/">Home</router-link>
       &nbsp;
       <router-link to="/about">About</router-link>
+      &nbsp;
+      <router-link to="/dynamic/happy">happy</router-link>
+      &nbsp;
+      <router-link to="/dynamic/exciting">exciting</router-link>
     </nav>
     <!--  동적 컴포넌트와 유사하게 작동한다.  해당 router-view에 따라 화면의 출력 위치가 달라진다. -->
   </div>
   <router-view></router-view>
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
-  <RouterView />
   <RouterView />
 </template>
 
--- 03_Vue.js/Vue-08-99/10.1-3-vue-router/router-vue/src/router/index.js
+++ 03_Vue.js/Vue-08-99/10.1-3-vue-router/router-vue/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
+import Dynamic from '@/pages/Dynamic.vue';
+import NotFound from '@/pages/NotFound.vue';
+
 /// 절대 경로
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +18,13 @@ const router = createRouter({
       // 직접 익명 함수로 연결
       component: () => import('@/pages/About.vue'),
     },
+    {
+      path: '/dynamic/:mood', // dynamic 뒤에 오는 값을 mode라는 파라미터로 받기로 했다.
+      name: 'Dynamic',
+      component: Dynamic,
+      props: true,
+    },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }, // 무조건 맨 아래에 작성
   ],
 });
 
