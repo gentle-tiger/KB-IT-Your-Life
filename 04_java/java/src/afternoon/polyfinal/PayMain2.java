b9344e994d67a792b3e335b79245b48b85bc67cc
Choi ho jin <jkhh9725@gmail.com>
2024-06-30 12:00:00 +0900

    PayMain 캡슐화

--- 04_java/java/src/afternoon/polyfinal/PayMain2.java
+++ 04_java/java/src/afternoon/polyfinal/PayMain2.java
@@ -1,7 +1,7 @@
 package afternoon.polyfinal;
 
 import java.util.Scanner;
-
+// PayMain 캡슐화
 public class PayMain2 {
     public static void main(String[] args) {
         PaySystem2 paySystem2 = new PaySystem2();
