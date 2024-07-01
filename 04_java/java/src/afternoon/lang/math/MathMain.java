6609a3982ad649b5778f7faaee6f3e77e6247768
Choi ho jin <jkhh9725@gmail.com>
2024-07-01 17:44:50 +0900

    2024.07.01 Math 메서드

---
+++ 04_java/java/src/afternoon/lang/math/MathMain.java
@@ -0,0 +1,22 @@
+package afternoon.lang.math;
+// Math 메서드
+public class MathMain {
+    public static void main(String[] args) {
+//       기본 연산
+        System.out.println(Math.max(10, 20));
+        System.out.println(Math.min(10, 20));
+        System.out.println(Math.abs(-10));
+
+//      소수점 연산
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.floor(2.1));
+        System.out.println(Math.round(2.1));
+
+//        기타
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.random()); // 0 <= random < 1
+
+
+
+    }
+}
