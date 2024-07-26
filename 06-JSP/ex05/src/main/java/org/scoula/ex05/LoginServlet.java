78c8b152d8341d75d303e51be8da36b1335e9f7e
Choi ho jin <jkhh9725@gmail.com>
2024-07-26 12:00:00 +0900

    2024.07.25 과제 평가

---
+++ 06-JSP/ex05/src/main/java/org/scoula/ex05/LoginServlet.java
@@ -0,0 +1,31 @@
+package org.scoula.ex05;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // request.getParameter
+        String userid = request.getParameter("userid");
+        String passwd = request.getParameter("passwd");
+
+        // request.setAttribute
+        request.setAttribute("userid", userid);
+        request.setAttribute("passwd", passwd);
+
+        // request.getRequestDispatcher.forward
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ 06-JSP/ex05/src/main/webapp/scope.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+        <h1>scope 데이터 보기</h1>
+        pageScope의 속성 값은 : ${pageScope.scopeName} <br/>
+        requestScope의 속성 값은 : ${requestScope.scopeName} <br/>
+        applicationScope의 속성 값은 : ${applicationScope.scopeName} <br/>
+
+        <!--  // request 가 리턴됨-->
+        scopeName 자동 찾기 : ${scopeName} <br/>
+ <!-- 실질적으로 getter를 사용하여 멤버에 접근하는 것이다. -->
+        member: ${member.name} (${member.userid}) <br/>
+
+  </body>
+</html>
